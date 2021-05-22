1224f39f270bad373cd4966e00438ef793322fa2
Quang <quangtd22@fpt.com.vn>
2021-05-22 11:51:01 +0700

    deploy

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, abort, request, make_response, url_for, render_template
+from flask import Flask, request
 from flask_restful import Api
 import pymongo
 from bson.objectid import ObjectId
@@ -8,7 +8,8 @@ app = Flask(__name__)
 api = Api(app)
 cors = CORS(app)
 
-myclient = pymongo.MongoClient('mongodb://localhost:27017/')
+# myclient = pymongo.MongoClient('mongodb://localhost:27017/')
+myclient = pymongo.MongoClient("mongodb+srv://quang123:quang123@appshop.fk9pu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 mydb = myclient['android']
 mycol = mydb['user']
 
@@ -19,10 +20,11 @@ def hello():
 @app.route('/signup/<username>/<email>/<password>')
 @cross_origin()
 def save_data_user(username, email, password):
+
     x = mycol.find_one({"username": str(username)})
     if x != None:
         return {
-            'Status': 'Error'
+            'status': 'Error'
         }
     mycol.insert_one({"_id": ObjectId(),"username": str(username), "email": str(email), "password": str(password)})
     return {
@@ -34,11 +36,47 @@ def check_data_user(username, password):
     x = mycol.find_one({"username": str(username)})
     if x['password'] == password:
         return {
-            'Status': 'success',
+            'status': 'success',
         }
     return {
         'Error': 'Not found user'
     }
+
+@app.route('/signups')
+@cross_origin()
+def save_data():
+    api_key = request.values.get('api_key')
+    username = request.values.get('username')
+    email = request.values.get('email')
+    password = request.values.get('password')
+
+    if api_key == '46ec83c05e17370e71c1ad416e76efd1':
+        x = mycol.find_one({"username": str(username)})
+        if x != None:
+            return {
+                'status': 'Error'
+            }
+        mycol.insert_one({"_id": ObjectId(),"username": str(username), "email": str(email), "password": str(password)})
+        return {
+            'Status': 'success',
+        }
+
+@app.route('/signins')
+@cross_origin()
+def check_data():
+    api_key = request.values.get('api_key')
+    username = request.values.get('username')
+    password = request.values.get('password')
+
+    if api_key == '46ec83c05e17370e71c1ad416e76efd1':
+        x = mycol.find_one({"username": str(username)})
+        if x['password'] == password:
+            return {
+                'status': 'success',
+            }
+        return {
+            'Error': 'Not found user'
+        }
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5005)
