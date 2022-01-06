a64a1745a64a10a4695587d9a421584e6bafc0eb
Duc-Quang <quang9538@gmail.com>
2022-01-06 15:14:26 +0700

    complete version

---
+++ constants.py
@@ -0,0 +1,14 @@
+import os
+
+HOST_SERVERS = "https://genxshopping.herokuapp.com"
+
+PATH_PROJECT = os.path.dirname(__file__)
+
+PATH_PRODUCT = "static/img/product/"
+
+PATH_MONGO = ""
+
+HOST_MAIL = ""
+
+PASS_MAIL = ""
+
