915ca1acd875de67eaf44cf9dda2c8ddf9e16994
Duc-Quang <quang9538@gmail.com>
2022-01-22 18:09:32 +0700

    add mongo & requirement

--- constants.py
+++ constants.py
@@ -4,7 +4,8 @@ import os
 
 PATH_PROJECT = os.path.dirname(__file__)
 
-PATH_MONGO = "mongodb://localhost:27017/" 
+# PATH_MONGO = "mongodb://localhost:27017/" 
+PATH_MONGO = "mongodb+srv://quangdb:quangdb@cluster0.36b1a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 QR_FOLDER = PATH_PROJECT + "/static/img/qrcode"
 
